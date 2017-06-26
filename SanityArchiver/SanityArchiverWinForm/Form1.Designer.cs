1f63da4d82af272f5fcdeb70e7a430aa7c9bdb58
Judit <tolnai.judit12@gmail.com>
2017-06-26 16:43:42 +0200

    filebrowser

---
+++ SanityArchiver/SanityArchiverWinForm/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace SanityArchiverWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Browse = new System.Windows.Forms.Button();
+            this.FileBrowser = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.Directories = new System.Windows.Forms.Label();
+            this.Files = new System.Windows.Forms.Label();
+            this.ActualDirectoriesName = new System.Windows.Forms.TextBox();
+            this.ActualDirectory = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Browse
+            // 
+            this.Browse.Location = new System.Drawing.Point(323, 1);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(75, 23);
+            this.Browse.TabIndex = 0;
+            this.Browse.Text = "Browse Files";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FileBrowser
+            // 
+            this.FileBrowser.Location = new System.Drawing.Point(275, 39);
+            this.FileBrowser.Name = "FileBrowser";
+            this.FileBrowser.Size = new System.Drawing.Size(171, 46);
+            this.FileBrowser.TabIndex = 1;
+            this.FileBrowser.Text = "FileBrowser";
+            this.FileBrowser.UseVisualStyleBackColor = true;
+            this.FileBrowser.Click += new System.EventHandler(this.FileBrowser_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(48, 163);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(225, 155);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(447, 163);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(244, 155);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // Directories
+            // 
+            this.Directories.AutoSize = true;
+            this.Directories.Location = new System.Drawing.Point(146, 106);
+            this.Directories.Name = "Directories";
+            this.Directories.Size = new System.Drawing.Size(57, 13);
+            this.Directories.TabIndex = 3;
+            this.Directories.Text = "Directories";
+            // 
+            // Files
+            // 
+            this.Files.AutoSize = true;
+            this.Files.Location = new System.Drawing.Point(548, 106);
+            this.Files.Name = "Files";
+            this.Files.Size = new System.Drawing.Size(28, 13);
+            this.Files.TabIndex = 4;
+            this.Files.Text = "Files";
+            // 
+            // ActualDirectoriesName
+            // 
+            this.ActualDirectoriesName.Location = new System.Drawing.Point(133, 137);
+            this.ActualDirectoriesName.Name = "ActualDirectoriesName";
+            this.ActualDirectoriesName.Size = new System.Drawing.Size(140, 20);
+            this.ActualDirectoriesName.TabIndex = 5;
+            this.ActualDirectoriesName.TextChanged += new System.EventHandler(this.ActualDirectoriesName_TextChanged);
+            // 
+            // ActualDirectory
+            // 
+            this.ActualDirectory.AutoSize = true;
+            this.ActualDirectory.Location = new System.Drawing.Point(45, 137);
+            this.ActualDirectory.Name = "ActualDirectory";
+            this.ActualDirectory.Size = new System.Drawing.Size(82, 13);
+            this.ActualDirectory.TabIndex = 6;
+            this.ActualDirectory.Text = "Actual Directory";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 396);
+            this.Controls.Add(this.ActualDirectory);
+            this.Controls.Add(this.ActualDirectoriesName);
+            this.Controls.Add(this.Files);
+            this.Controls.Add(this.Directories);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.FileBrowser);
+            this.Controls.Add(this.Browse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.Button FileBrowser;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label Directories;
+        private System.Windows.Forms.Label Files;
+        private System.Windows.Forms.TextBox ActualDirectoriesName;
+        private System.Windows.Forms.Label ActualDirectory;
+    }
+}
+
---
+++ SanityArchiver/SanityArchiverWinForm/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SanityArchiverWinForm
+{
+    public partial class Form1 : Form
+    {
+        static List<FileInfo> FoundFiles;
+        static string directoryName = @"C:\Users\Judit";
+        static string fileName;
+        DirectoryInfo directory;
+        DirectoryInfo[] directories;
+        FileInfo[] files;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Show the dialog and get result.
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+            }
+            Console.WriteLine(result); // <-- For debugging use.
+        }
+
+        private void FileBrowser_Click(object sender, EventArgs e)
+        {
+            directory = new DirectoryInfo(directoryName);
+            directories = directory.GetDirectories();
+            files = directory.GetFiles();
+
+            tableLayoutPanel1.AutoSize = (directories.Length > 16) ? false : true;
+            tableLayoutPanel1.AutoScroll = (directories.Length > 16) ? true: false;
+
+            tableLayoutPanel2.AutoSize = (files.Length > 16) ? false : true;
+            tableLayoutPanel2.AutoScroll = (files.Length > 16) ? true : false;
+            CreateMapButtons(directories.Length, directories, files.Length, files);
+
+            ActualDirectoriesName.Text = directoryName;
+        }
+
+        public void CreateMapButtons(int DirectoryNumber, DirectoryInfo[] directories, int FileNumber, FileInfo[] files)
+        {
+            int rowCount;
+            int rowCountForFiles;
+            int columnCount = 4;
+            
+
+            rowCount = (DirectoryNumber % columnCount == 0) ? DirectoryNumber % columnCount : (DirectoryNumber % columnCount) + 1;
+            rowCountForFiles = (FileNumber % columnCount == 0) ? FileNumber % columnCount : (FileNumber % columnCount) + 1;
+
+            this.tableLayoutPanel1.ColumnCount = columnCount;
+            this.tableLayoutPanel1.RowCount = rowCount;
+
+            this.tableLayoutPanel1.ColumnStyles.Clear();
+            this.tableLayoutPanel1.RowStyles.Clear();
+
+            for (int i = 0; i < columnCount; i++)
+            {
+                this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100 / columnCount));
+                this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100 / columnCount));
+            }
+            for (int i = 0; i < rowCount; i++)
+            {
+                this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100 / rowCount));
+            }
+            for (int i = 0; i < rowCountForFiles; i++)
+            {
+                this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100 / rowCount));
+            }
+
+            foreach (DirectoryInfo directory in directories)
+            {
+                var dir = new Button();
+                dir.Text = (directory.Name).ToString();
+                dir.Name = string.Format("dir_{0}", (directory.Name).ToString());
+                dir.Click += dir_Click;
+                dir.Dock = DockStyle.Fill;
+                this.tableLayoutPanel1.Controls.Add(dir);
+            }
+
+            foreach (FileInfo file in files)
+            {
+                var fil = new Button();
+                fil.Text = (file.Name).ToString();
+                fil.Name = string.Format("fil_{0}", (file.Name).ToString());
+                fil.Click += fil_Click;
+                fil.Dock = DockStyle.Fill;
+                this.tableLayoutPanel2.Controls.Add(fil);
+            }
+
+        }
+
+        void dir_Click(object sender, EventArgs e)
+        {
+            var b = sender as Button;
+            if (b != null)
+                MessageBox.Show(string.Format("{0} Clicked", b.Text));
+
+            directoryName = (sender as Button).Text;
+            directory = new DirectoryInfo(directoryName);
+            directories = directory.GetDirectories();
+            files = directory.GetFiles();
+
+            tableLayoutPanel1.AutoSize = (directories.Length > 16) ? false : true;
+            tableLayoutPanel1.AutoScroll = (directories.Length > 16) ? true : false;
+
+            tableLayoutPanel2.AutoSize = (files.Length > 16) ? false : true;
+            tableLayoutPanel2.AutoScroll = (files.Length > 16) ? true : false;
+            CreateMapButtons(directories.Length, directories, files.Length, files);
+
+            ActualDirectoriesName.Text = directoryName;
+        }
+
+        void fil_Click(object sender, EventArgs e)
+        {
+            var b = sender as Button;
+            if (b != null)
+                MessageBox.Show(string.Format("{0} Clicked", b.Text));
+        }
+
+        static void RecursiveSearch(List<FileInfo> foundFiles, string fileName, DirectoryInfo currentDirectory)
+        {
+            foreach (FileInfo fil in currentDirectory.GetFiles())
+            {
+                if (fil.Name == fileName)
+                    foundFiles.Add(fil);
+            }
+            foreach (DirectoryInfo dir in currentDirectory.GetDirectories())
+            {
+                RecursiveSearch(foundFiles, fileName, dir);
+            }
+        }
+
+        private void flowLayoutPanel1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void ActualDirectoriesName_TextChanged(object sender, EventArgs e)
+        {
+            directoryName = (sender as TextBox).Text;
+            directory = new DirectoryInfo(directoryName);
+            directories = directory.GetDirectories();
+            files = directory.GetFiles();
+
+            tableLayoutPanel1.AutoSize = (directories.Length > 16) ? false : true;
+            tableLayoutPanel1.AutoScroll = (directories.Length > 16) ? true : false;
+
+            tableLayoutPanel2.AutoSize = (files.Length > 16) ? false : true;
+            tableLayoutPanel2.AutoScroll = (files.Length > 16) ? true : false;
+            CreateMapButtons(directories.Length, directories, files.Length, files);
+        }
+    }
+}
